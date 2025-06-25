a3873c6accda87af1f3759c6195b8de5a4efc730
MeguminShiro <MeguminShiro124@outlook.com>
2025-06-25 13:05:36 -0300

    Create event.js
    Easier manipulation of events date/time update with this newly created file.

---
+++ event.js
@@ -0,0 +1,5 @@
+window.AWA_CFG = {
+  chosenTime:  new Date("2025/06/20 17:00:00 UTC").getTime(),
+  promoTime:   new Date("2025/06/23 00:00:00 UTC").getTime(),
+  SCELinkBase: `${location.origin}/steam/community-event/dead-by-daylight-free-trial-community-event`
+};
